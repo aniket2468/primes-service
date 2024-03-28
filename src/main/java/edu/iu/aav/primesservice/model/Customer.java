3c5a63b6feeba4b2d2cb8fcc79b5aa43e15925b9
Aniket Sharma <aniketsharma2468@gmail.com>
2024-03-28 16:01:01 -0400

    praticum 12

--- src/main/java/edu/iu/aav/primesservice/model/Customer.java
+++ src/main/java/edu/iu/aav/primesservice/model/Customer.java
@@ -7,13 +7,13 @@ public class Customer {
         this.username = username;
         this.password = password;
     }
-    public String getUsername() {return username;}
+
+    public String getUsername() { return username; }
+
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getPassword() {return password;}
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
